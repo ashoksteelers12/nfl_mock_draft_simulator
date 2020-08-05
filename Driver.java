d789aeea7b81805454fe132ef65cd0558f901924
Ashok Meyyappan <61124747+ashoksteelers12@users.noreply.github.com>
2020-08-05 15:22:19 -0500

    Delete Driver.java

--- Driver.java
+++
@@ -1,9 +0,0 @@
-
-public class Driver 
-{
-	public static void main(String[] args) 
-	{
-		NFLDraftSimulator nds = new NFLDraftSimulator(); //Declaring Draft Simulator Object
-		nds.simulateDraft(); //Performing Mock Draft Simulation
-	}  
-}
